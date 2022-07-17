30f3f5adbd83b6fde8cdfb352d9f28cad7335a24
Svet2018 <bulatova44@gmail.com>
2022-07-17 21:26:54 +0300

    Added Palindrome() method

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+// 14212 -> нет, 12821 -> да, 23432 -> да
+
+void Palindrome(int number)
+{
+    string word = Convert.ToString(number);
+    int wordLenght = word.Length;
+    string newWord = String.Empty;
+
+    for(int i = 0; i < wordLenght; i++)
+    {
+        char s = word[i];
+        System.Console.WriteLine(s); 
+
+      // System.Console.Write(word[i]);
+       
+
+       //System.Console.WriteLine("+++++++++++++++");  
+       
+    }  
+
+    /*
+    if (word == newWord)
+    {
+       System.Console.WriteLine($"Дано число {number}, которое является палиндромом."); 
+    }
+    else
+    {
+        System.Console.WriteLine($"Дано число {number}, которое не является палиндромом.");
+    }
+    */
+
+}
+
+Palindrome(987654);
