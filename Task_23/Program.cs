28b4fa23a844fc9a20d9e35b159ce67b2e2f5db6
Svet2018 <bulatova44@gmail.com>
2022-07-18 19:58:04 +0300

    Added a solution to task No. 13

---
+++ Task_23/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+/*
+int[] CubeNumber(int num)
+{
+    int[] array1 = new int[num];
+    for(int i = 0; i < array1.Length; i++)
+    {
+        int cube = num * num * num;
+        array1.SetValue(cube, i);
+        System.Console.WriteLine(array1);
+    }
+
+
+}
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+CubeNumber(number);
+*/
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array1 = new int[num];
+for(int i = 0; i < array1.Length; i++)
+{
+    int cube = num * num * num;
+    array1.SetValue(cube, i);
+    System.Console.WriteLine(array1);
+}
