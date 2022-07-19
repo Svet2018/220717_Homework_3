0b5abcb47b0f9ceb37ee61586327facfaf77703f
Svet2018 <bulatova44@gmail.com>
2022-07-19 21:01:03 +0300

    Added a solution to task No. 23

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -21,13 +21,28 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 CubeNumber(number);
 */
-
+/*
 System.Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int[] array1 = new int[num];
 for(int i = 0; i < array1.Length; i++)
 {
-    int cube = num * num * num;
-    array1.SetValue(cube, i);
-    System.Console.WriteLine(array1);
+    int cube = (i+1)*(i+1)*(i+1);
+    System.Console.Write(cube);
+    System.Console.Write(", ");
+}
+*/
+//2 Решение задачи:
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < num; i++)
+{
+    int cube = (i+1)*(i+1)*(i+1);
+     System.Console.Write($"{cube}, "); 
+
+    //Возвести в степень можно другим способом:
+
+    //int cub = Convert.ToInt32(Math.Pow(i + 1, 3));
+    //System.Console.Write($"{cub}, ");
 }
