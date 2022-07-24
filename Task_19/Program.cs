58c5ffdd37dfa4143674253d68a50b9522dd84e4
Svet2018 <bulatova44@gmail.com>
2022-07-24 20:20:57 +0300

    Added a solution to task No. 19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,35 +1,65 @@
 ﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
 // 14212 -> нет, 12821 -> да, 23432 -> да
 
-void Palindrome(int number)
+//Решение №1 задачи.
+
+
+System.Console.WriteLine("Введите число:");
+string word = Console.ReadLine();
+int wordLenght = word.Length;
+string wordReverse = "";
+
+for (int i = word.Length - 1; i >= 0; i--)
 {
-    string word = Convert.ToString(number);
-    int wordLenght = word.Length;
-    string newWord = String.Empty;
+    wordReverse += word[i];
+};
 
-    for(int i = 0; i < wordLenght; i++)
-    {
-        char s = word[i];
-        System.Console.WriteLine(s); 
-
-      // System.Console.Write(word[i]);
-       
-
-       //System.Console.WriteLine("+++++++++++++++");  
-       
-    }  
-
-    /*
-    if (word == newWord)
-    {
-       System.Console.WriteLine($"Дано число {number}, которое является палиндромом."); 
-    }
-    else
-    {
-        System.Console.WriteLine($"Дано число {number}, которое не является палиндромом.");
-    }
-    */
-
+if (wordReverse == word)
+{
+    System.Console.WriteLine("Введенное Вами число является палиндромом.");
 }
+else
+{
+    System.Console.WriteLine("Введенное Вами число не является палиндромом.");
+};
 
-Palindrome(987654);
+
+//Решение №2 задачи.
+
+/*
+System.Console.WriteLine("Введите число:");
+string word = Console.ReadLine();
+string startHalfWord;
+string endHalfWord;
+int wordLenght = word.Length;
+int middleWord = wordLenght / 2;
+int moreMiddleWorld = middleWord + 1;
+string outputEnd = "";
+
+if (wordLenght % 2 == 0)
+{
+    startHalfWord = word.Substring(0, middleWord);
+    endHalfWord = word.Substring(middleWord);
+}
+else 
+{
+    startHalfWord = word.Substring(0, moreMiddleWorld);    
+    endHalfWord = word.Substring(middleWord);
+};
+
+for (int i = endHalfWord.Length - 1; i >= 0; i--)
+{
+    outputEnd += endHalfWord[i];
+};
+
+if (outputEnd == startHalfWord)
+{
+    System.Console.WriteLine("Введенное Вами число является палиндромом.");
+}
+else
+{
+    System.Console.WriteLine("Введенное Вами число не является палиндромом.");
+};
+*/
+
+
