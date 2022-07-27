516b3ba930dd872eef275f80b86da01885d5dec4
Svet2018 <bulatova44@gmail.com>
2022-07-27 22:52:48 +0300

    The final decision of the task

--- Task_21/Program.cs
+++ Task_21/Program.cs
@@ -2,11 +2,50 @@
 //A (3,6,8); B (2,1,-7), -> 15.84
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 
-int distanceBetweenPoints(int xx, int yy, int zz)
+System.Console.WriteLine();
+System.Console.WriteLine("Введите координаты первой точки:");
+System.Console.WriteLine();
+System.Console.WriteLine("Введите x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+
+System.Console.WriteLine("Введите координаты второй точки:");
+System.Console.WriteLine();
+System.Console.WriteLine("Введите x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+
+int Sq(int arg1,  int arg2)
+{ 
+    return (arg2 - arg1)*(arg2 - arg1);
+};
+
+int SumResalt(int arg1, int arg2, int arg3)
 {
+    return arg1 + arg2 + arg3;
+};
+
+double SqResalt(int arg1)
+{
+    return Math.Sqrt(arg1); 
+};
+
+double resalt = SqResalt(SumResalt(Sq(x1, x2), Sq(y1, y2), Sq(z1, z2)));
+
+System.Console.WriteLine("Расстояние  между двумя точками в 3D пространстве равно: {0:F2}", resalt);
 
 
+/*
 
-}
-
-distanceBetweenPoints(num1 , num2);
+double a = 4.5646743;
+Console.WriteLine("{0:F3}",a);
+Console.WriteLine("{0:F2}",a);
+*/
