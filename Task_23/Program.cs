7d31c1b7d9ff514573d646b9282f15e4ecf44e50
Svet2018 <bulatova44@gmail.com>
2022-07-28 06:53:35 +0300

    The last desigion of the task

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -4,23 +4,44 @@
 
 //Решение №1 задачи:
 
+/*
+
 System.Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 for(int i = 0; i < num; i++)
 {
-    int cube = (i+1)*(i+1)*(i+1); 
-
-    //Возвести в степень можно другим способом:
-
-    //int cub = Convert.ToInt32(Math.Pow(i + 1, 3));
-    
-    System.Console.Write($"{cube}, ");
+    int cube = (i+1)*(i+1)*(i+1);     
+    System.Console.Write(cube +  " ");
 }
 
 System.Console.WriteLine();
 
+*/
 
 
+//Решение №2 универсальное для степени числа
 
+/*
+
+System.Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите степень числа:");
+int numPow = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+
+int Sq(int arg1, int arg2)
+{
+    return Convert.ToInt32(Math.Pow(arg1,arg2));
+}
+
+while (number <= num)
+{
+    System.Console.Write(Sq(number, numPow) + " ");
+    number++;  
+}
+
+System.Console.WriteLine();
+
+*/
 
 
